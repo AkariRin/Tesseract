276eab6d62036b1a7d7fba84e2f224cd39bbf9d1
梦清 <akaririn1028@gmail.com>
2025-12-26 03:54:42 +0800

    更新超维立方属性

--- src/main/java/dev/rbq/tesseract/Tesseract.java
+++ src/main/java/dev/rbq/tesseract/Tesseract.java
@@ -6,9 +6,11 @@ import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Rarity;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.MapColor;
+import net.minecraft.world.level.material.PushReaction;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
@@ -28,9 +30,21 @@ public class Tesseract {
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MODID);
 
     // 超维立方：方块
-    public static final RegistryObject<Block> TESSERACT_BLOCK = BLOCKS.register("tesseract", () -> new Block(BlockBehaviour.Properties.of().mapColor(MapColor.METAL).strength(5.0f, 6.0f).requiresCorrectToolForDrops()));
+    public static final RegistryObject<Block> TESSERACT_BLOCK = BLOCKS.register("tesseract", () -> new Block(
+            BlockBehaviour.Properties.of()
+                    .mapColor(MapColor.METAL)
+                    .strength(100.0f, 3600000.0f) // 硬度100，爆炸抗性3600000
+                    .requiresCorrectToolForDrops() // 需要合适的工具
+                    .lightLevel(state -> 15) // 亮度15
+                    .pushReaction(PushReaction.BLOCK) // 不可被活塞推动拉动
+                    .isRedstoneConductor((state, getter, pos) -> true) // 红石导体
+    ));
     // 超维立方：物品
-    public static final RegistryObject<Item> TESSERACT_BLOCK_ITEM = ITEMS.register("tesseract", () -> new BlockItem(TESSERACT_BLOCK.get(), new Item.Properties()));
+    public static final RegistryObject<Item> TESSERACT_BLOCK_ITEM = ITEMS.register("tesseract", () -> new BlockItem(TESSERACT_BLOCK.get(),
+            new Item.Properties()
+                    .stacksTo(1) // 最大堆叠为1
+                    .rarity(Rarity.EPIC) // 史诗级稀有度
+    ));
 
     // 创造模式标签页
     public static final RegistryObject<CreativeModeTab> TESSERACT_TAB = CREATIVE_MODE_TABS.register("tesseract_tab", () -> CreativeModeTab.builder()
